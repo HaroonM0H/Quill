e1bd84b11777def9ae8ac55b277bac9a9751ae49
Haroon Mohammed <88782074+HaroonM0H@users.noreply.github.com>
2025-05-06 18:27:42 +0100

    added functional chat rooms, local message history preservation

--- server.js
+++ server.js
@@ -1,24 +1,69 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
-
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server);
+
+let users = [];
+
+const messages = {
+    "general": [],
+    "coding": [],
+    "Jokes": []
+}
 
 app.use(express.static('public'))
 
 io.on('connection', socket =>{
-    console.log('User Connected successfully')
-    console.log(socket.id)
+    console.log('User '+ socket.id + ' Connected successfully')
+    
+    socket.on("user-joined", (username) => {
+        const user = {
+            username,
+            id: socket.id,
+        };
+        users.push(user);
+        io.emit("new-user", users);
+    })
 
-    socket.on("chat-message", (msg) =>{
-        io.emit("chat-message", msg);
+    socket.on("join-room", (roomName, cb) => {
+        socket.join(roomName);
+        cb(messages[roomName]);
+    })
+
+    socket.on("send-message", ({content, to, sender, chatName, isChannel }) => {
+        if (isChannel) {
+            const payload = {
+                content,
+                chatName,
+                sender
+            };
+            socket.to(to).emit("new-message", payload);
+        }else {
+            const payload = {
+                content,
+                chatName: sender,
+                sender
+            };
+            socket.to(to).emit("new-message", payload);
+        }
+        if (messages[chatName]) {
+            messages[chatName].push({
+                content,
+                sender
+            });
+        }
+    })
+
+    socket.on("chat-message", (msg, client) =>{
+        io.emit("chat-message", msg, client);
     })
 
     //listening for disconnection
     socket.on('disconnect', () =>{
-        console.log('User Disconnected')
+        users = users.filter(user => user.id !== socket.id);
+        io.emit("new-user", users)
     })
 })
 
